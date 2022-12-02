388754e7398dbebf130257625f5223ddda4194bb
Tom Hughes <tomhughes1987@gmail.com>
2022-12-02 18:31:07 +0100

    site fully functioning

--- src/components/SearchBarExpanding/index.tsx
+++ src/components/SearchBarExpanding/index.tsx
@@ -4,12 +4,20 @@ import { useEffect } from "react";
 import searchIcon from "./SearchIcon.svg";
 import App from "../../App";
 
-export default function SearchBarExpanding() {
+export default function SearchBarExpanding(props: any) {
   // let [testingfromsearch,settestingfromsearch]=useState(0)
   //     function goClicked() {
   //       console.log('go clicked')
   //     }
   // const childRef= React.createRef();
+
+  function setSearch() {
+    let searchInout = document.getElementById(
+      "searchinput"
+    ) as HTMLInputElement;
+    props.setSearchWord(searchInout.value);
+  }
+
   return (
     <>
       <br />
@@ -17,7 +25,9 @@ export default function SearchBarExpanding() {
         <div id="SearchBar-front">
           <img src={searchIcon} id="search-icon" alt="Search Icon" />
           <input id="searchinput" autoComplete="off"></input>
-          <p id="search-btn-text">Go</p>
+          <p id="search-btn-text" onClick={setSearch}>
+            Go
+          </p>
         </div>
       </div>
     </>
